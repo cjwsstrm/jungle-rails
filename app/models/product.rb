622eb3814c13d096478cf749be12b815fbe3b942
cjwsstrm <cjwesstrom@gmail.com>
2017-10-16 22:08:32 +0000

    added specs for product property  validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,16 @@
 class Product < ActiveRecord::Base
 
   has_many :reviews
+  belongs_to :category
 
   monetize :price_cents, numericality: true
   mount_uploader :image, ProductImageUploader
 
-  belongs_to :category
 
   validates :name, presence: true
   validates :price, presence: true
   validates :quantity, presence: true
-  validates :quantity, numericality: { greater_than: 0 }
+  # validates :quantity, numericality: { greater_than: 0 }
   validates :category, presence: true
 
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    it ('should be invalid without a name') do
+      product = Product.new
+    expect(product).to_not be_valid
+      expect(product.errors.messages[:name]).to include('can\'t be blank')
+    end
+    
+    it ('should be invalid without a price') do
+      product = Product.new
+      expect(product).to_not be_valid
+      expect(product.errors.messages[:price]).to include('can\'t be blank')
+    end
+
+    it ('should be invalid without a quantity') do
+      product = Product.new
+      expect(product).to_not be_valid
+      expect(product.errors.messages[:quantity]).to include('can\'t be blank')
+    end
+
+    it ('should be invalid without a category') do
+      product = Product.new
+      expect(product).to_not be_valid
+      expect(product.errors.messages[:category]).to include('can\'t be blank')
+    end
+      
+    it ('should be valid with all validations met') do
+        category = Category.new(name: 'Test')
+        product = Product.new(name: 'CJ', price: 15, quantity: 5, category: category)
+        expect(product).to be_valid
+        expect(product.errors.messages)
+    end
+  end
+end
